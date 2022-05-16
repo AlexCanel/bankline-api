6465b5756d973298147384a785c1473662abbf96
AlexCanel <Alexcanelprofissional@gmail.com>
2022-05-15 22:16:35 -0300

    Finalizando back

--- src/main/java/com/dio/santander/banklineapi/model/Movimentacao.java
+++ src/main/java/com/dio/santander/banklineapi/model/Movimentacao.java
@@ -11,6 +11,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Entity
 @Table(name= "tab_movimentacao")
 public class Movimentacao {
@@ -18,8 +20,6 @@ public class Movimentacao {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
-	@Column(name = "data_hora")
-	private LocalDateTime dataHora;
 	
 	private String descricao;
 	
@@ -30,6 +30,10 @@ public class Movimentacao {
 	
 	@Column(name = "id_conta")
 	private Integer idConta;
+	
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@Column(name = "data_hora")
+	private LocalDateTime dataHora;
 	
 	public Integer getId() {
 		return id;
